f287da13f74424479914ed6a7c4a555633b7b7cc
Thomas Wolfe <tewolfe2@gmail.com>
2016-09-23 12:32:16 -0700

    cleaned up files

--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -5,16 +5,6 @@ import NavigationDrawer from './NavigationDrawer'
 import NavItems from './NavItems'
 
 // screens identified by the router
-import PresentationScreen from '../Containers/PresentationScreen'
-import AllComponentsScreen from '../Containers/AllComponentsScreen'
-import UsageExamplesScreen from '../Containers/UsageExamplesScreen'
-import LoginScreen from '../Containers/LoginScreen'
-import ListviewExample from '../Containers/ListviewExample'
-import ListviewGridExample from '../Containers/ListviewGridExample'
-import MapviewExample from '../Containers/MapviewExample'
-import APITestingScreen from '../Containers/APITestingScreen'
-import ThemeScreen from '../Containers/ThemeScreen'
-import DeviceInfoScreen from '../Containers/DeviceInfoScreen'
 import Scanner from '../Containers/Scanner'
 
 /* **************************
--- App/Reducers/index.js
+++ App/Reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux'
-import LoginReducer from './LoginReducer'
-import WeatherReducer from './WeatherReducer'
 import UPCReducer from './UPCReducer'
 
 // glue all the reducers together into 1 root reducer
 export default combineReducers({
-  login: LoginReducer,
-  weather: WeatherReducer,
   upc: UPCReducer
 })
 
